d1c70f398200ca5f52b07cff78608b7661a0230f
Naima <naimamohamed19@hotmail.com>
2022-04-23 16:38:29 -0400

    adding start and end times

--- lib/screens/home/alarms_screen.dart
+++ lib/screens/home/alarms_screen.dart
@@ -56,12 +56,22 @@ class _HomeScreenState extends State<HomeScreen> {
               final DocumentSnapshot doc = docs[index];
               Alarm alarm = Alarm.fromDocument(doc);
               String freqSubtitle = ' ';
-              if (alarm.freq_num == 0) {
-                freqSubtitle = 'Once';
-              } else {
-                freqSubtitle = 'Every ' + alarm.freq_num.toString() + ' ' + alarm.freq_unit;
+              DateTime start_time = alarm.start_time;
+              DateTime? end_time = alarm.end_time;
+
+              // change date format to be more readable: kk:mm:a dd-mm
+              String startTime = "${start_time.hour}:${start_time.minute.toString().padLeft(2, '0')} ${start_time.hour > 12 ? 'PM' : 'AM'} ${start_time.day.toString().padLeft(2, '0')}-${start_time.month.toString().padLeft(2, '0')}-${start_time.year}";
+              String endTime = " ";
+              if (end_time != null) {
+                endTime = "${end_time.hour}:${end_time.minute.toString().padLeft(2, '0')} ${end_time.hour > 12 ? 'PM' : 'AM'} ${end_time.day.toString().padLeft(2, '0')}-${end_time.month.toString().padLeft(2, '0')}-${end_time.year}";
               }
 
+              if (alarm.freq_num == 0) {
+                freqSubtitle = 'Once at ' + startTime;
+              } else {
+                freqSubtitle = 'Every ' + alarm.freq_num.toString() + ' ' + alarm.freq_unit + 'at ' + startTime + ' to ' + endTime;
+              }
+             
 
               return Card(
                   child: ListTile(
